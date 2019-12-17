d97c6aa2bd77a87bea0b7664075ace116827f668
Ridzi Hafiz <ridzihafiz@gmail.com>
2019-12-17 22:05:22 +0700

    routes client and port 3004

--- client/routes/index.js
+++ client/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res) {
+  res.render('home');
+});
+
+router.get('/data', function(req, res) {
+  res.render('data');
+});
+
+router.get('/data-date', function(req, res) {
+  res.render('data-date');
+});
+
+router.get('/maps', function(req, res) {
+  res.render('maps');
+});
+
 module.exports = router;
